308c9a4b09188fdd1d087a0fd4a160e19800035c
Maryana <mar1610alex@gmail.com>
2023-01-03 23:39:48 +0300

    Число(N)-все целые числа в промежутке -N до N

---
+++ Seminar_1/Console_5 ChisloN-N/Program.cs
@@ -0,0 +1,12 @@
+﻿// №5 Напиите программу, кот-ая на вход принимает одно число (N), а на 
+// выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Введите число: ");
+int  number = Convert.ToInt32(Console.ReadLine());
+int startNumber = 0 - number;
+
+while (startNumber != number + 1)
+{
+    Console.Write(startNumber);
+    startNumber ++;
+}
