2b6a3dd33b91f1129417065d3bef868e37c59105
Maryana <mar1610alex@gmail.com>
2023-01-03 23:27:35 +0300

    Вход трехзначнЧисло-послЦифраЧисла

---
+++ Seminar_1/Console_4 PoslCifrChisla/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, кот-ая принимает на вход трехзначное число и 
+// на выходе показ-ет посл цифру этого числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(number % 10);
