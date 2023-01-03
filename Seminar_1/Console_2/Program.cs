9c3b95785623d9b85866ee22f34869bd937a4ac7
Maryana <mar1610alex@gmail.com>
2023-01-03 22:48:34 +0300

    вход два чилсла-является ли первое квадратом второго

---
+++ Seminar_1/Console_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// №1 Напиите программу, кот-ая на вход принимает два числа и 
+// проверяет, является ли первое число квадратом второго.
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Первое число является квадратом второго ");
+}
+else
+{
+  Console.WriteLine("Первое число НЕ является квадратом второго ");  
+}
