24f5d692d687882341f0fd7748eca6ebfc97f7fd
Maryana <mar1610alex@gmail.com>
2023-01-05 01:15:38 +0300

    1 случ число отезка 10-99 -наибольшая цифра числа

---
+++ Seminar_2/ConsoleApp_1 sl ch otrezka/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, кот-ая выводит случайное число из отрезка [10,99] 
+   и показывает наибольшую цифру числа  */
+
+//случайное число
+int number = 54;
+
+//первая цифра числа
+int firstDigit = number/10;
+//вторая цифра числа
+int secondDigit = number%10;
+
+Console.WriteLine($"Случайное число: {number}");
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа: {firstDigit}");
+}
+if (secondDigit > firstDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа: {secondDigit}");
+}
+if (secondDigit == firstDigit)
+{
+    Console.WriteLine($"Цифры в числе равны");
+}
