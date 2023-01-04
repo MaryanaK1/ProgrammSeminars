f14f0f01d1bfdc94b85a364d629bd67a7905f10a
Maryana <mar1610alex@gmail.com>
2023-01-05 02:12:17 +0300

    зад11 случ трехзн число-удал втор цифра числа

---
+++ Seminar_2/ConsoleZadacha11/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, кот-ая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+
+int number = new Random().Next(100,1000);
+
+int firstDigit = number/100;
+int thirdDigit = number%10;
+int finalNumber = firstDigit*10 + thirdDigit;
+
+Console.WriteLine($"Случайное трехзначное число {number}, после удаления средней цифры которого поучается {finalNumber}");
+
+Console.Read();
