f9095369964902d2c64f2352f87fcb4cf4958bb3
Maryana <mar1610alex@gmail.com>
2023-01-05 01:46:21 +0300

    1.1 случ число из отрезка 10-99 -наиб цифра числа(ГЕНЕРАТОР случ чисел)

---
+++ Seminar_2/ConsoleApp_1 sl ch otrezka generator/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, кот-ая выводит случайное число из отрезка [10,99] 
+   и показывает наибольшую цифру числа  */
+
+//генератор случайного числа
+int number = new Random().Next(10,100);
+
+int firstDigit = number/10;
+int secondDigit = number%10;
+
+Console.WriteLine($"Случайное число: {number}");
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа: {firstDigit}");
+}
+if (secondDigit > firstDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа: {secondDigit}");
+}
+if (secondDigit == firstDigit)
+{
+    Console.WriteLine($"Цифры в числе равны");
+}
