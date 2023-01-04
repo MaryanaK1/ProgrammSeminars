b73156573ea78c5041e5161d452d15165417a48e
Maryana <mar1610alex@gmail.com>
2023-01-04 22:46:39 +0300

    3_6 вход число-четное или нет

---
+++ DZ Seminar_1/Zadanie3_6/Program.cs
@@ -0,0 +1,16 @@
+﻿// №6 Напишите программу, которая на вход принимает число и выдает, 
+//    является ли число четным (делится ли оно на два без остатка)
+
+Console.WriteLine("Четное ли число?");
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number%2 == 0)
+{
+    Console.WriteLine($"Число {number} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {number} не является четным");
+}
