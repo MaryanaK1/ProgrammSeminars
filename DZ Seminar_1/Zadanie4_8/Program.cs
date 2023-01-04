65e04953c73345ffba5dff1f01199e0070a654a1
Maryana <mar1610alex@gmail.com>
2023-01-04 23:44:34 +0300

    4_8 на вход (N)-четн числа от 1 до N

---
+++ DZ Seminar_1/Zadanie4_8/Program.cs
@@ -0,0 +1,16 @@
+﻿/* №8 Напишите программу, которая на вход принимае число (N), 
+      а на выходе показываетвсе четные числа от 1 до N   */
+
+Console.WriteLine("Введите положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int startNumber = 1;
+
+
+while (startNumber <= number )
+   if (startNumber % 2 == 0)
+{
+    Console.Write($"{startNumber}, ");
+    startNumber ++;
+}
+else startNumber++;
