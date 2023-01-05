d9d5977e0be86c9c28a4c52c58835be596859d9c
Maryana <mar1610alex@gmail.com>
2023-01-06 00:21:24 +0300

    случ трехзн число-удалить ср цифру

---
+++ Seminar_2/ConsoleZadacha11 3hzn ch ud vt Cfr/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, кот-ая выводит случайное трехзначное число 
+   и удаляет вторую цифру этого числа */
+
+int number = new Random().Next(100,1000);
+
+int thirdDigit = number%10;
+
+int firstDigit = number/100;
+
+int result = firstDigit*10 + thirdDigit;
+
+Console.WriteLine($"Случайное трехзначное число {number}");
+
+Console.Write($"После удаления средней цифры получается {result}");
+Console.Read();
