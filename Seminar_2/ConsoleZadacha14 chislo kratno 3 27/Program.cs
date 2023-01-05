34e7d7c4bb79eb7c79428f90761e934eead37e10
Maryana <mar1610alex@gmail.com>
2023-01-06 01:26:58 +0300

    №14 число-красто 3 и 27

---
+++ Seminar_2/ConsoleZadacha14 chislo kratno 3 27/Program.cs
@@ -0,0 +1,12 @@
+﻿//#14 Напишите программу, кот-ая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number %7 == 0 && number %23 == 0)
+{
+    Console.WriteLine($"{number} кратно одновременно 3 и 27. ");
+}
+else
+{
+    Console.WriteLine($"{number} не кратно ");
+}
