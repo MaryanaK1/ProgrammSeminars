e035d6b9e613afd423e4ae9198ae221071d566a3
Maryana <mar1610alex@gmail.com>
2023-01-07 00:32:11 +0300

    №15.1 ДеньНед(цифрой)-выходной да/нет

---
+++ DZ Seminar_2/Zadacha3_15.1/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+        6 -> да
+        7 -> да
+        1 -> нет */
+
+Console.WriteLine("Введие номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("нет");
+}
+else if (number == 2)
+{
+    Console.WriteLine("нет"); 
+}
+else if (number == 3)
+{
+    Console.WriteLine("нет"); 
+}
+else if (number == 4)
+{
+    Console.WriteLine("нет"); 
+}
+else if (number == 5)
+{
+    Console.WriteLine("нет"); 
+}
+else if (number == 6)
+{
+    Console.WriteLine("Да"); 
+}
+else if (number == 7)
+{
+    Console.WriteLine("Да"); 
+}
+else
+{
+    Console.WriteLine("Такого дня недели не существует!");  
+}
