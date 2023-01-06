1dadf0a5ed46d1376bface5611e2df7f33e9b64c
Maryana <mar1610alex@gmail.com>
2023-01-06 23:22:02 +0300

    13(string) вывод 3-тья цифра числа

---
+++ DZ Seminar_2/Zadacha2_13/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+                    645 -> 5
+                    78 -> третьей цифры нет
+                    32679 -> 6                           
+
+Console.WriteLine("Введите целое число: ");
+string number = Console.ReadLine();
+ */
+
+Console.WriteLine("Введите целое число: ");
+string number = Console.ReadLine();
+int userNumber = int.Parse(number);
+
+if (number.Length >= 3)
+{
+    Console.WriteLine($"Третяя цифра числа {userNumber} это {number[2]}");
+    return;
+}
+else if (number.Length < 3)
+{
+    Console.WriteLine("Третьей цифры нет");
+     Console.WriteLine("Введите трехзначное или более число");
+}   
