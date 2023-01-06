52ef5d40b6bf308e409e4ff5cc7c1da65c492f67
Maryana <mar1610alex@gmail.com>
2023-01-06 23:57:52 +0300

    №3(case) число{1-7} -День недели

---
+++ Seminar_1/Console_3.1 DenNed Case/Program.cs
@@ -0,0 +1,28 @@
+﻿// №3 Напишите программу,кот-ая будет выдавать название дня недели по заданному номеру
+
+Console.Write("Введие число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch(number) {
+case 1:
+        Console.WriteLine("Понедельник");
+        break;
+case 2:
+        Console.WriteLine("Вторник");
+        break;
+case 3:
+        Console.WriteLine("Среда");
+        break;
+case 4:
+        Console.WriteLine("Четверг");
+        break;
+case 5:
+        Console.WriteLine("Пятница");
+        break;
+case 6:
+        Console.WriteLine("Суббота");
+        break;
+case 7:
+        Console.WriteLine("Воскресение");
+        break;
+}
