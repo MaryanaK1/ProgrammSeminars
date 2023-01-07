4e56a8d13f1e287f98b7e819e316708085550d92
Maryana <mar1610alex@gmail.com>
2023-01-07 19:53:20 +0300

    №10.1 трехзн число-вторая цифра

---
+++ DZ Seminar_2/Zadacha1_10.1 uluchsheno/Program.cs
@@ -0,0 +1,22 @@
+﻿// №10 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int num = 0;
+Console.WriteLine("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+if (number < 0) {num = number*(-1);}
+else
+num = number;
+
+if (num >= 100 && num <= 999)
+{
+int firstDigit = num/10;
+int thirdDigit = firstDigit%10;
+    num = thirdDigit;
+    Console.WriteLine($"Вторая цифра числа {number} это {num} ");
+}
+else
+{
+  Console.WriteLine("Вы ввели не трехзначное число"); 
+   Console.WriteLine("Введите трехзначное число"); 
+}
