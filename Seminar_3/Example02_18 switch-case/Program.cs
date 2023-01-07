ac53581248eaef228bc41ba304672fee7c6a1d9e
Maryana <mar1610alex@gmail.com>
2023-01-08 01:40:31 +0300

    №18 номер четверти-диапазон коорд

---
+++ Seminar_3/Example02_18 switch-case/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон 
+// возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите номер четверти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch(number)
+{
+    case 1:
+        Console.WriteLine($"Значение координат {number}й четверти: X>0 Y>0");
+        break;
+
+    case 2:
+        Console.WriteLine($"Значение координат {number}й четверти: X<0 Y>0");
+        break;
+
+    case 3:
+        Console.WriteLine($"Значение координат {number}й четверти: X<0 Y<0");
+        break;
+
+    case 4:
+        Console.WriteLine($"Значение координат {number}й четверти: X>0 Y<0");
+        break;
+
+    default:
+        Console.WriteLine("Такой четверти не существует!");
+        break;
+}
