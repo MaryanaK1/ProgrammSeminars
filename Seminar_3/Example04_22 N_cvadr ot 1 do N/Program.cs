8ab8e9c4316836535c385c45868cbac79f0074b7
Maryana <mar1610alex@gmail.com>
2023-01-10 17:11:00 +0300

    №22 циклы While|For/числоN-квадраты от 1доN

---
+++ Seminar_3/Example04_22 N_cvadr ot 1 do N/Program.cs
@@ -0,0 +1,36 @@
+﻿/* №22 Напишите программу, которая принимает на вход число (N) и выдаёт 
+таблицу квадратов чисел от 1 до N.
+            5 -> 1, 4, 9, 16, 25.
+            2 -> 1,4                    */
+/*
+Console.Write("Сколько раз вывести слово ПРИВЕТ:");
+int count = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+while(i <= count)
+{
+   Console.WriteLine($"Привет! [{i}]");
+    i = i +1;
+}
+*/
+/*
+Console.Write("Сколько раз вывести слово ПРИВЕТ:");
+int count = Convert.ToInt32(Console.ReadLine());
+ for (int i = 1; i <= count; i++)
+ {
+    if (i == 3)
+    {
+        break;
+    }
+    Console.WriteLine($"Привет! [{i}]");
+ }
+ */
+ //принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"Квадрат числа {i} = {i * i}");
+}
