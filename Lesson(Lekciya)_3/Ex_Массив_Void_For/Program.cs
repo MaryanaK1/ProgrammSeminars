e56108e335a66cac41673e5ad9738bbc56e7f4b4
Maryana <mar1610alex@gmail.com>
2023-01-11 21:45:29 +0300

    Метод.Массив чисел-упорядочить от мин/макс

---
+++ Lesson(Lekciya)_3/Ex_Массив_Void_For/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort (int[] array)  //метод, который будет упорядочивать массив
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int minPosition = i;
+        for (int j = i +1; j < array.Length; j++)  //ищем минимальный эл-нт
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];         //обмен двух переменных местами
+        array[i] = array[minPosition];
+        array[minPosition] = temporary; 
+    }
+}
+PrintArray(arr);
+SelectionSort (arr);
+
+PrintArray(arr);
