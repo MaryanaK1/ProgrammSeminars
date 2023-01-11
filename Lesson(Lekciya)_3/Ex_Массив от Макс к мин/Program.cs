e66a601660412252658efc1364ec221483b1b444
Maryana <mar1610alex@gmail.com>
2023-01-11 22:29:57 +0300

    массив чисел -от макс к мин(метод)

---
+++ Lesson(Lekciya)_3/Ex_Массив от Макс к мин/Program.cs
@@ -0,0 +1,29 @@
+﻿//упорядочить массив чиссел от МАКС к МИН
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort (int[] array)  //метод, который будет упорядочивать массив
+{
+    for (int i = 0; i < array.Length ; i++)
+    {
+        int maxPosition = i;
+        for (int j = i +1; j < array.Length; j++)  //ищем максимальный эл-нт
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[maxPosition];         //обмен двух переменных местами
+        array[maxPosition] = array[i];
+        array[i] = temporary; 
+    }
+}
+PrintArray(arr);
+SelectionSort (arr);
+
+PrintArray(arr);
