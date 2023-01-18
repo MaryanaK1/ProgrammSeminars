616811ce682d04f279e6d7d249e7cf001fe8cbaa
Maryana <mar1610alex@gmail.com>
2023-01-18 18:39:37 +0300

    №28 факториал

---
+++ Seminar_4/Ex_28_factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿/* №28 Напишите программу, кот-ая на вход принимает число N и выдает произведение чисел от 1 до N
+4-> 24
+5 -> 120   */
+
+Console.WriteLine("Input numer: ");
+string answer = Console.ReadLine();
+int number = Convert.ToInt32(answer);
+
+int factorial = 1;
+if (number > 0)
+{
+    for(int i = 1; i <= number; i++)
+    {
+        factorial = factorial * i;
+    }
+    Console.WriteLine(factorial);
+}
+else Console.WriteLine("The integer is wrong");
