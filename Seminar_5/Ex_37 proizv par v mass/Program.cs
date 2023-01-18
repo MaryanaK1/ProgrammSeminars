e6c44c5b916df3f89b3e8823e412c0a8304d008f
Maryana <mar1610alex@gmail.com>
2023-01-19 01:40:42 +0300

    №37 произведение пар чисел в одномерном массиве

---
+++ Seminar_5/Ex_37 proizv par v mass/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый 
+  и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+        [1 2 3 4 5] -> 5 8 3
+        [6 7 3 6] -> 36 21  */
+/*
+int[] array = {1,2,3,4,5};
+int size = (array.Length + 1) / 2;
+
+int[] newArray = new int[size];
+for(int i = 0; i < array.Length / 2; i++)
+{
+    newArray[i] = array[i] * array[array.Length - 1 - i];
+}
+
+if(array.Length % 2 == 1)
+{
+    newArray[size - 1] = array[array.Length / 2];
+}
+Console.WriteLine(string.Join(", ", newArray));   */
+int[] array = {1,2,4,8,5};
+int size = (array.Length+1)/2;
+/*
+int size;
+if(array.Length % 2 == 0)
+{
+    size = array.Length/ 2;
+}
+else
+{
+    size = array.Length/2 +1;
+}
+*/
+int[] newArray = new int[size];
+for(int i = 0; i < newArray.Length; i++)
+{
+    newArray[i] = array[i] * array[array.Length - 1 - i];
+}
+if(array.Length % 2 == 1)  //если был эл-нт посередине
+{
+    newArray[size - 1] = array[array.Length / 2];
+}
+Console.WriteLine(string.Join(", ", newArray)); 
