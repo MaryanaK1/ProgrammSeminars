5da0d6b58aad403e0ff2ab145a9cbd3351940ed0
Maryana <mar1610alex@gmail.com>
2023-01-18 22:09:51 +0300

    #29 массив(8)от пользователя

---
+++ DZ Seminar_4/Zadacha 3_29/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и 
+// выводит их на экран. Данные вводятся с консоли пользователем
+
+int [] CollectionArray(int size)
+{
+    int[] arr = new int [size];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите {i+1}-й элемент массива: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine()); 
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    foreach(var el in arr)
+    {
+        Console.Write(el + "  ");
+    }
+}
+int[] array = CollectionArray(8);
+PrintArray(array);
+/*
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = GreateArray(8);
+PrintArray(array); */
