197393c209f574cd9f982b5971968f828a2ba30e
Maryana <mar1610alex@gmail.com>
2023-01-24 21:22:51 +0300

    #42 десятичн число в двоичн

--- Seminar_6/Ex_42 10tichn ch v 2ichn ch/Program.cs
+++ Seminar_6/Ex_42 10tichn ch v 2ichn ch/Program.cs
@@ -30,3 +30,30 @@ Console.WriteLine($"Число {decimalNumber} в десятичной сист�
 //     N = N/2;
 // }
 // Console.WriteLine(b);
+
+/*
+string binSys(int num)
+{
+    int tempNum = 0;
+    string binNum = "";
+    while(num != 0)
+        {
+            tempNum = num % 2;
+            num /= 2;
+            binNum += tempNum;
+        }
+        
+    string revBin = "";
+    int num_rev = Int32.Parse(binNum);
+    int temp = 0;
+    for(int idx = binNum.Length; idx > 0; idx--)
+        {
+            temp = num_rev % 10;
+            num_rev /= 10;
+            revBin += temp.ToString();
+        }
+    return revBin;
+}
+
+Console.WriteLine(binSys(3));
+*/
