553a83f9b098ef204ba89ead42fd4cf561f85214
Maryana <mar1610alex@gmail.com>
2023-01-24 16:39:04 +0300

    №39 разворо одномерн массива/метод рандом заполнения

---
+++ Seminar_6/Ex_39 переворот одномрн массива/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+//            [1 2 3 4 5] -> [5 4 3 2 1]
+//            [6 7 3 6] -> [6 3 7 6]
+  //метод (GetRandomArray) генерации случ массива
+int [] GetRandomArray(nint size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(leftRange, rightRange+1);
+    }
+    return array;
+}
+void Reverse(int[] sourceArray)
+{
+    for(int i = 0; i < sourceArray.Length / 2; i++)
+    {
+        Console.WriteLine($"Индекс слева {i}\tИндекс справа {sourceArray.Length - i - 1}");
+        int temp = sourceArray[i];
+        sourceArray[i] = sourceArray[sourceArray.Length - i - 1];
+        sourceArray[sourceArray.Length - i - 1] = temp;
+    }
+}
+const int size = 5;
+const int leftRange = -9;
+const int rightRange = 9;
+int[] massive = GetRandomArray (size, leftRange, rightRange);  // int[] massive = GetRandomArray (12, -9, 9);
+Console.WriteLine(string.Join(", ", massive));
+
+Reverse(massive);
+Console.WriteLine(string.Join(", ", massive));
