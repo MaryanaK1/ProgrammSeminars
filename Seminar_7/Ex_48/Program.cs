339e8554796745ad41a81f92dafbed30d7f9bfd8
Maryana <mar1610alex@gmail.com>
2023-02-01 14:44:01 +0300

    №48 2мернМасс Amn=m+n

---
+++ Seminar_7/Ex_48/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+
+int [,] GetMatrix (int m, int n)
+{
+    int [,] matrix = new int [m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix [i,j] = i + j ;
+        }
+    }
+    return matrix; 
+}
+void PrintMatrix (int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix [i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+const int Rows = 3;
+const int Columns = 4;
+int [,] array = GetMatrix(Rows, Columns);
+PrintMatrix(array);
