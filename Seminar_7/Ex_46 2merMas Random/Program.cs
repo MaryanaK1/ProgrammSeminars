27c59abedc909e57a94f02a7fac666b8adfd690a
Maryana <mar1610alex@gmail.com>
2023-02-01 14:25:37 +0300

    №46 2мерн массив Рандом

---
+++ Seminar_7/Ex_46 2merMas Random/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int [,] GetRandomMatrix (int rows, int columns, int leftRange, int rightRange)
+{
+    int [,] matrix = new int [rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix [i,j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return matrix; 
+}
+const int Rows = 3;
+const int Columns = 4;
+const int LEFTrange = 0;
+const int RIGHTrange = 10;
+
+void PrintMatrix (int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix [i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int [,] array = GetRandomMatrix(Rows, Columns, LEFTrange, RIGHTrange);
+PrintMatrix(array);
